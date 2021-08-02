e7a54c345cbe3d1a2eab03b0aa524a4a1b5b3c7c
Coremotor <coremotor@users.noreply.github.com>
2021-08-02 09:22:24 +0300

    start

--- api/unsplash.ts
+++ api/unsplash.ts
@@ -1,5 +1,6 @@
 import { createApi } from 'unsplash-js';
 
 export const unsplash = createApi({
-  accessKey: process.env.UNSPLASH_KEY ? process.env.UNSPLASH_KEY : '',
+  // accessKey: process.env.UNSPLASH_KEY ? process.env.UNSPLASH_KEY : '',
+  accessKey: 'qSOpebaEAZ8yxG5AhJJPdrvnGEAveSJtgUl7EbzdkRc',
 });
